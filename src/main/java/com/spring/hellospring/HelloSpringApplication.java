91daf8db8be24ddc4cf1b4e0da680ae2e6cf30ab
bhawna-singh20 <6368anshi@gmail.com>
2022-01-17 14:44:37 +0530

    my-commit

---
+++ src/main/java/com/spring/hellospring/HelloSpringApplication.java
@@ -0,0 +1,16 @@
+package com.spring.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class HelloSpringApplication {
+@RequestMapping("/Spring")
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
